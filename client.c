bcc547763ad0a6f2335c336e64ce0dbd09928ebb
Jascar87 <riccardopidello@gmail.com>
2017-05-05 20:26:17 +0200

    prima battuta

--- client.c
+++ client.c
@@ -1,6 +1,6 @@
-#include <stdio.h>      
+#include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>   
+#include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,23 +13,26 @@ int main(int argc, char *argv[]) {
     int simplePort = 0;
     int returnStatus = 0;
     char buffer[256] = "";
+    char keyword[4]= "";
     struct sockaddr_in simpleServer;
+    const char MESSAGE_OK[] = "OK ";
+    const char MESSAGE_NO[] = "NO ";
+    const char MESSAGE_SI[] = "SI ";
+    const char MESSAGE_ER[] = "ER ";
+
+    int end=0; /** flag per terminare il ciclo while*/
 
     if (3 != argc) {
-
-        fprintf(stderr, "Usage: %s <server> <port>\n", argv[0]);
-        exit(1);
-
+      fprintf(stderr, "Usage: %s <server> <port>\n", argv[0]);
+      exit(1);
     }
 
     /* create a streaming socket      */
     simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     if (simpleSocket == -1) {
-
-        fprintf(stderr, "Could not create a socket!\n");
-        exit(1);
-
+      fprintf(stderr, "Could not create a socket!\n");
+      exit(1);
     }
     else {
 	    fprintf(stderr, "Socket created!\n");
@@ -40,7 +43,7 @@ int main(int argc, char *argv[]) {
 
     /* setup the address structure */
     /* use the IP address sent as an argument for the server address  */
-    //bzero(&simpleServer, sizeof(simpleServer)); 
+    //bzero(&simpleServer, sizeof(simpleServer));
     memset(&simpleServer, '\0', sizeof(simpleServer));
     simpleServer.sin_family = AF_INET;
     //inet_addr(argv[2], &simpleServer.sin_addr.s_addr);
@@ -54,22 +57,68 @@ int main(int argc, char *argv[]) {
 	    fprintf(stderr, "Connect successful!\n");
     }
     else {
-        fprintf(stderr, "Could not connect to address!\n");
-	close(simpleSocket);
-	exit(1);
+      fprintf(stderr, "Could not connect to address!\n");
+      close(simpleSocket);
+      exit(1);
     }
 
     /* get the message from the server   */
-    returnStatus = read(simpleSocket, buffer, sizeof(buffer));
-
+    returnStatus = read(simpleSocket, buffer, sizeof(buffer));/** per leggere il messa di benvenuto*/
     if ( returnStatus > 0 ) {
+      read(buffer, keyword, sizeof(keyword));
+      if(strcmp(keyword, MESSAGE_OK)==0){/**ramo in cui il server ha risposto con il messaggio di benvenuto in modo corretto*/
         printf("%d: %s", returnStatus, buffer);
-    } else {
+        while(end==0){
+          printf("Inserire un numero da 1 a 100\n");
+          printf("tutti gli altri valori termineranno il gioco\n", );
+          fflush(stdin);
+          fscanf(stdin, "%s", buffer);
+          write(simplePort, buffer, strlen(buffer)); /**inoltro il messaggio conetenuto in buffer al servver*/
+
+          do{ /**continuo il ciclo fino a quando riesco ad effettuare una read andata a buon fine*/
+            returnStatus = read(simpleSocket, buffer, sizeof(buffer));
+            if ( returnStatus < 0 ) fprintf(stderr, "Return Status = %d \n", returnStatus);
+          }while(returnStatus>0);
+
+            /**valutare il messaggio ricevuto dal server*/
+          read(buffer, keyword, sizeof(keyword));
+          if(strcmp(keyword, MESSAGE_SI)==0){/**ramo in cui il numero è stato indovinato*/
+            end=1;
+            printf("%s", buffer);
+            printf("Sono il client migliore che ci sia, ti ho fatto indovinare\n");
+          }
+
+          if(strcmp(keyword, MESSAGE_ER)==0){/**ramo in cui ci sono stati errori o si sono superati i tentativi massimi*/
+            end=1;
+            printf("%s", buffer);
+            printf("Se il valore inserito era errato, il server se ne e' accorto\n");
+            printf("Se invece non hai indovinato, non e' colpa mia, io ho svolto il mio compito corretamente\n");
+          }
+
+          if(strcmp(keyword, MESSAGE_NO)==0){/**ramo in cui il numero non e' stato indovinato*/
+            read(buffer, keyword, 1);
+            if(strcmp(keyword, "-")==0) printf("il numero inserito e' MAGGIORE del numero da indovinare \n");/**il numero inserito e' maggiore del numero da indovinare*/
+            if(strcmp(keyword, "+")==0) printf("il numero inserito e' MINORE del numero da indovinare \n");/**il numero inserito e' minore del numero da indovinare*/
+            else {/**la parola chiave inviata non e' codificata correttamente*/
+              fprintf(stderr, "Parola chiave trasmessa : %s \n", keyword);
+              end=1;
+            }
+          }
+
+          else{/**casi restanti*/
+            else fprintf(stderr, "Parola chiave trasmessa : %s \n", keyword);
+            end=1;
+          }
+
+        }
+
+      }
+      else fprintf(stderr, "Parola chiave trasmessa : %s \n", keyword); /**ramo in cui la parola chiave non risulta corretta, viene mandata in output*/
+    }
+    else {/**ramo in cui ci sono stati problemi con la read*/
         fprintf(stderr, "Return Status = %d \n", returnStatus);
     }
 
     close(simpleSocket);
     return 0;
-
 }
-
--- server.c
+++ server.c
@@ -1,12 +1,18 @@
-#include <stdio.h>      
+#include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>   
+#include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> 
+#include <unistd.h>
+#define TENTATIVI_MAX 7
 
-const char MESSAGE[] = "Hello UPO student!\n";
+const char MESSAGE_WELCOME[] = "OK benvenuti nel server GIAVAZZI - PIDELLO\n";
+const char MESSAGE_MINOR[] = "NO +\n";
+const char MESSAGE_MAJOR[] = "NO -\n";
+const char MESSAGE_CORRECT[] = "SI complimenti HAI INDOVINATO !!!\n";
+const char MESSAGE_ERROR[] = "ER valore NON ACCETTABILE dal server\n";
+const char MESSAGE_ERROR_TENTATIVI[] = "ER numero di tentativi esauriti, RITENTA sarai piu' fortunato\n";
 
 int main(int argc, char *argv[]) {
 
@@ -14,21 +20,27 @@ int main(int argc, char *argv[]) {
     int simplePort = 0;
     int returnStatus = 0;
     struct sockaddr_in simpleServer;
+    int random=0;
+    int tentativo=0;
+    int valore_client=0;/**variabile in cui viene registrato il numero fornito dal cliente come tentativo*/
+    int i=0;
+    int end=0; /**flag per intrrompere il ciclo di verifica del valore fornito dal client se indovinato o se il valore non e' accettabile*/
+    struct sockaddr_in clientName = { 0 };
+    int simpleChildSocket = 0;
+    int clientNameLength = 0;
+    char buffer[32] = "";
+    char output[64] = "";
 
     if (2 != argc) {
-
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(1);
-
+      fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+      exit(1);
     }
 
     simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     if (simpleSocket == -1) {
-
-        fprintf(stderr, "Could not create a socket!\n");
-        exit(1);
-
+      fprintf(stderr, "Could not create a socket!\n");
+      exit(1);
     }
     else {
 	    fprintf(stderr, "Socket created!\n");
@@ -39,7 +51,7 @@ int main(int argc, char *argv[]) {
 
     /* setup the address structure */
     /* use INADDR_ANY to bind to all local addresses  */
-    memset(&simpleServer, '\0', sizeof(simpleServer)); 
+    memset(&simpleServer, '\0', sizeof(simpleServer));
     simpleServer.sin_family = AF_INET;
     simpleServer.sin_addr.s_addr = htonl(INADDR_ANY);
     simpleServer.sin_port = htons(simplePort);
@@ -51,49 +63,74 @@ int main(int argc, char *argv[]) {
 	    fprintf(stderr, "Bind completed!\n");
     }
     else {
-        fprintf(stderr, "Could not bind to address!\n");
-	close(simpleSocket);
-	exit(1);
+      fprintf(stderr, "Could not bind to address!\n");
+      close(simpleSocket);
+      exit(1);
     }
 
     /* lets listen on the socket for connections      */
     returnStatus = listen(simpleSocket, 5);
 
     if (returnStatus == -1) {
-        fprintf(stderr, "Cannot listen on socket!\n");
-	close(simpleSocket);
-        exit(1);
+      fprintf(stderr, "Cannot listen on socket!\n");
+      close(simpleSocket);
+      exit(1);
     }
 
     while (1)
 
     {
+      simpleChildSocket = 0;
+      sockaddr_in clientName = { 0 };
 
-        struct sockaddr_in clientName = { 0 };
-	int simpleChildSocket = 0;
-	int clientNameLength = sizeof(clientName);
-
+      while(simpleChildSocket<=0){
+        simpleChildSocket = accept(simpleSocket,(struct sockaddr *)&clientName, &clientNameLength);
+        clientNameLength = sizeof(clientName);
+        if (simpleChildSocket == -1) {
+          fprintf(stderr, "Cannot accept connections!\n");
+        }
+      }
 	/* wait here */
 
-        simpleChildSocket = accept(simpleSocket,(struct sockaddr *)&clientName, &clientNameLength);
-
-	if (simpleChildSocket == -1) {
-
-            fprintf(stderr, "Cannot accept connections!\n");
-	    close(simpleSocket);
-	    exit(1);
-
-	}
 
         /* handle the new connection request  */
 	/* write out our message to the client */
-	write(simpleChildSocket, MESSAGE, strlen(MESSAGE));
-        close(simpleChildSocket);
+      write(simpleChildSocket, MESSAGE_WELCOME, strlen(MESSAGE_WELCOME));
 
+      while(tentativo++<TENTATIVI_MAX && end=0){
+        srand(time(NULL)); /**modifico il seme di random*/
+        random = rand()%100 + 1; /**genero il numero casuale compreso tra 1 e 100*/
+
+        do{
+          returnStatus = read(simpleSocket, buffer, sizeof(buffer));
+        }while(returnStatus>0);
+
+        valore_client=atoi(buffer)
+
+        if(valore_client==random){/** ramo in cui il numero e' corretto*/
+          strcpy(output, MESSAGE_CORRECT);
+          end=1;
+        }
+        else if(valore_client> 0 && valore_client<random){/** ramo in cui il numero e' minore di random*/
+          strcpy(output, MESSAGE_MINOR);
+        }
+        else if (valore_client > random && valore_client <= 100){ /** ramo in cui il numero e' maggiore di random */
+          strcpy(output, MESSAGE_MAJOR)
+        }
+        else { /** ramo in cui il numero non e' accettabile*/
+          strcpy(output, MESSAGE_ERROR);
+          end=1;
+        }
+        write(simpleChildSocket, output, strlen(output));
+      }
+
+      if(tentativo==TENTATIVI_MAX){
+        write(simpleChildSocket, MESSAGE_ERROR_TENTATIVI, strlen(MESSAGE_ERROR_TENTATIVI));
+      }
+
+      close(simpleChildSocket);
     }
 
     close(simpleSocket);
     return 0;
-
 }
-
